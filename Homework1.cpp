cf095551cdb3708c151d6fe79b000fa769744312
firstfractal <73749403+firstfractal@users.noreply.github.com>
2024-09-23 22:02:36 +0800

    Create Homework1.cpp

---
+++ Homework1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    int x;
+    int y;
+    int sum = 0;
+    cout << "Please input two numbers to add:" << endl;
+    cin >> x >> y;
+    sum = x + y;
+    cout << "Result: " << sum << endl;
+
+    cout << "Please input two numbers to subtract:" << endl;
+    cin >> x >> y;
+    cout << "Result: " <<  x - y << endl;
+
+
+    return 0;
+
+
+}
+
+
+
